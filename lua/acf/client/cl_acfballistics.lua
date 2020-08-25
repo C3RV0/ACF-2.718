1ebb20ce0c0770da70da9ad69b87bd3d65cc486f
Ani Lakiyari <51520657+C3RV0@users.noreply.github.com>
2020-08-24 21:56:48 -0700

    Added comments to cl_acfballistics.lua

--- lua/acf/client/cl_acfballistics.lua
+++ lua/acf/client/cl_acfballistics.lua
@@ -1,9 +1,14 @@
+--[[
+	Clientside effect positioning for ACF rounds
+	A few extra comments for better readability. Nothing else really needs changing.
+]]--
+
 ACF.BulletEffect = {}
 
 function ACF_ManageBulletEffects()
 	
 	for Index,Bullet in pairs(ACF.BulletEffect) do
-		ACF_SimBulletFlight( Bullet, Index )			--This is the bullet entry in the table, the omnipresent Index var refers to this
+		ACF_SimBulletFlight( Bullet, Index )	-- This is the bullet entry in the table, the omnipresent Index var refers to this
 	end
 	
 end
@@ -11,19 +16,22 @@ hook.Add("Think", "ACF_ManageBulletEffects", ACF_ManageBulletEffects)
 
 function ACF_SimBulletFlight( Bullet, Index )
 
-	--local DeltaTime = ACF.CurTime - Bullet.LastThink
-	local DeltaTime = CurTime() - Bullet.LastThink --intentionally not using cached curtime value
+	-- Compares the current time against the last time the bullet updated
+	local DeltaTime = CurTime() - Bullet.LastThink
 	
-	local Drag = Bullet.SimFlight:GetNormalized() * (Bullet.DragCoef * Bullet.SimFlight:Length()^2)/ACF.DragDiv
-	--print(Drag)
-	--debugoverlay.Cross(Bullet.SimPos,3,15,Color(255,255,255,32), true)
-	Bullet.SimPosLast = Bullet.SimPos
-	Bullet.SimPos = Bullet.SimPos + (Bullet.SimFlight * ACF.VelScale * DeltaTime)		--Calculates the next shell position
-	Bullet.SimFlight = Bullet.SimFlight + (Bullet.Accel - Drag)*DeltaTime			--Calculates the next shell vector
+	-- Gets the velocity, normalizes it and calculates the drag
+	local Drag = Bullet.SimFlight:GetNormalized() * (Bullet.DragCoef * Bullet.SimFlight:Length() ^ 2) / ACF.DragDiv
+	
+	Bullet.SimPosLast = Bullet.SimPos	-- Gets the current position of the bullet
+	
+	Bullet.SimPos = Bullet.SimPos + (Bullet.SimFlight * ACF.VelScale * DeltaTime)	-- Calculates the new position of the bullet
+	
+	Bullet.SimFlight = Bullet.SimFlight + (Bullet.Accel - Drag) * DeltaTime		-- Calculates the new velocity of the bullet
 	
 	if Bullet and Bullet.Effect:IsValid() then
 		Bullet.Effect:ApplyMovement( Bullet )
 	end
-	Bullet.LastThink = CurTime() --ACF.CurTime --intentionally not using cached curtime value
+	
+	Bullet.LastThink = CurTime()	-- Sets the "last updated" time of the bullet to the current time
 	
 end
