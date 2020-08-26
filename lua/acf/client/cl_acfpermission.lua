5a1e7a0ad725fe4036e78748d15d8071f150edff
Ani Lakiyari <51520657+C3RV0@users.noreply.github.com>
2020-08-26 12:24:18 -0700

    Added comments to "cl_acfpermission.lua"

--- lua/acf/client/cl_acfpermission.lua
+++ lua/acf/client/cl_acfpermission.lua
@@ -1,8 +1,16 @@
--- Code modified from the NADMOD client permissions menu, by Nebual
--- http://www.facepunch.com/showthread.php?t=1221183
+--[[ 
+	Code modified from the NADMOD client permissions menu, by Nebual
+	http://www.facepunch.com/showthread.php?t=1221183
+	
+	This is the Damage Permissions menu in the Utilities tab
+	It will be moved to the ACF tab when I get around to making it
+]]--
 
 
+-- Finds ACF table, or creates one if it does not exist
 ACF = ACF or {}
+
+-- Gets Permissions data from the ACF table, or creates it if it does not exist
 ACF.Permissions = ACF.Permissions or {}
 local this = ACF.Permissions
 
@@ -10,12 +18,11 @@ local getPanelChecks = function() return {} end
 
 
 
+-- Net message for updating the list of people on your Damage Buddies list
 net.Receive("ACF_refreshfriends", function(len)
 	--Msg("\ncl refreshfriends\n")
 	local perms = net.ReadTable()
 	local checks = getPanelChecks()
-	
-	--PrintTable(perms)
 	
 	for k, check in pairs(checks) do
 		if perms[check.steamid] then
@@ -28,7 +35,7 @@ net.Receive("ACF_refreshfriends", function(len)
 end)
 
 
-
+-- Net message to check whether your Damage Permissions were successfully updated or not
 net.Receive("ACF_refreshfeedback", function(len)
 	local success = net.ReadBit()
 	local str, notify
@@ -46,12 +53,15 @@ net.Receive("ACF_refreshfeedback", function(len)
 end)
 
 
-
+-- Net message that sends the buddies list to the server
 function this.ApplyPermissions(checks)
 	perms = {}
 	
 	for k, check in pairs(checks) do
-		if not check.steamid then Error("Encountered player checkbox without an attached SteamID!") end
+		-- Error thrown if a player is invalid
+		if not check.steamid then
+			Error("Encountered player checkbox without an attached SteamID!")
+		end
 		perms[check.steamid] = check:GetChecked()
 	end
 	
@@ -61,7 +71,7 @@ function this.ApplyPermissions(checks)
 end
 
 
-
+-- Function for making the actual panel
 function this.ClientPanel(Panel)
 	Panel:ClearControls()
 	if !this.ClientCPanel then this.ClientCPanel = Panel end
@@ -100,7 +110,7 @@ function this.ClientPanel(Panel)
 end
 
 
-
+-- Function for creating the panel when the spawn menu is opened
 function this.SpawnMenuOpen()
 	if this.ClientCPanel then
 		this.ClientPanel(this.ClientCPanel)
@@ -109,7 +119,7 @@ end
 hook.Add("SpawnMenuOpen", "ACFPermissionsSpawnMenuOpen", this.SpawnMenuOpen)
 
 
-
+-- Function for creating the options in the Utilities menu
 function this.PopulateToolMenu()
 	spawnmenu.AddToolMenuOption("Utilities", "ACF", "Damage Permission", "Damage Permission", "", "", this.ClientPanel)
 end
