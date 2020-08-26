1339fc54567030d1cf7ce70b406bf0883739d62b
Ani Lakiyari <51520657+C3RV0@users.noreply.github.com>
2020-08-25 21:04:36 -0700

    Fixed typo in "smoothbore.lua"

--- lua/acf/shared/guns/smoothbore.lua
+++ lua/acf/shared/guns/smoothbore.lua
@@ -42,7 +42,7 @@ ACF_defineGun("120mmSB", {
 } )
 	
 ACF_defineGun("140mmSB", {
-	name = "140mm Smoothbore Cannonn",
+	name = "140mm Smoothbore Cannon",
 	desc = "The 140mm fires a massive shell with enormous penetrative capability, but has a glacial reload speed and a very hefty weight.",
 	model = "models/tankgun_old/tankgun_140mm.mdl",
 	gunclass = "SB",
@@ -55,7 +55,6 @@ ACF_defineGun("140mmSB", {
 	}
 } )
 
---[[
 ACF_defineGun("170mmC", {
 	name = "170mm Cannon",
 	desc = "The 170mm fires a gigantic shell with ginormous penetrative capability, but has a glacial reload speed and an extremely hefty weight.",
@@ -69,4 +68,3 @@ ACF_defineGun("170mmC", {
 		propweight = 34
 	}
 } )
-]]--	
