6c0ac399c63e25e665a89b8301f5a986cd0d939f
Ani Lakiyari <51520657+C3RV0@users.noreply.github.com>
2020-09-02 08:21:45 -0700

    Fixed comments

--- lua/acf/server/permissionmodes/acf_pmode_safe.lua
+++ lua/acf/server/permissionmodes/acf_pmode_safe.lua
@@ -1,20 +1,20 @@
-/**
+--[[
 	ACF Permission mode: Safe
 		This mode completely disables damage upon everything.
-//*/
+]]--
 
 if not ACF or not ACF.Permissions or not ACF.Permissions.RegisterMode then error("ACF: Tried to load the " .. modename .. " permission-mode before the permission-core has loaded!") end
 local perms = ACF.Permissions
 
 
-// the name for this mode used in commands and identification
+-- The name for this mode used in commands and identification
 local modename = "safe"
 
-// a short description of what the mode does
+-- A short description of what the mode does
 local modedescription = "Completely disables damage upon everything."
 
 
-/*
+--[[
 	Defines the behaviour of ACF damage protection under this protection mode.
 	This function is called every time an entity can be affected by potential ACF damage.
 	Args;
@@ -23,7 +23,7 @@ local modedescription = "Completely disables damage upon everything."
 		ent			Entity:	The entity which may be damaged.
 	Return: boolean
 		true if the entity should be damaged, false if the entity should be protected from the damage.
-//*/
+]]--
 local function modepermission(owner, attacker, ent)	
 	return false
 end
